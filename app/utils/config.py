f6ed2279e0d373448877181612cd56c6541e3043
Santi G. <24705975+santiagoa58@users.noreply.github.com>
2023-12-17 18:15:03 +0000

    Add documentation

--- app/utils/config.py
+++ app/utils/config.py
@@ -6,10 +6,26 @@ load_dotenv()
 
 
 def get_env_variable(env_variable_name: str, default_value: str = "") -> str:
+    """
+    Retrieves the value of an environment variable.
+
+    Args:
+        env_variable_name (str): The name of the environment variable.
+        default_value (str, optional): The default value to return if the environment variable is not set. Defaults to "".
+
+    Returns:
+        str: The value of the environment variable, or the default value if not set.
+    """
     return os.getenv(env_variable_name, default_value)
 
 
 def configure_logging():
+    """
+    Configures the logging settings.
+
+    Returns:
+        logging.Logger: The logger object.
+    """
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s - %(levelname)s - %(message)s",
--- app/utils/server_util.py
+++ app/utils/server_util.py
@@ -4,6 +4,18 @@ from fastapi import HTTPException
 
 
 async def load_image_file(file):
+    """
+    Loads an image file from the given file object.
+
+    Args:
+        file: The file object representing the image file.
+
+    Returns:
+        The loaded PIL image.
+
+    Raises:
+        HTTPException: If the image file is invalid.
+    """
     if file.content_type.startswith("image/"):
         # Read image file and convert it to a PIL image
         image_data = await file.read()
@@ -15,7 +27,15 @@ async def load_image_file(file):
 
 
 def image_to_bytes(image):
-    """Converts a PIL image to a byte array"""
+    """
+    Converts a PIL image to a byte array.
+
+    Args:
+        image: The PIL image to be converted.
+
+    Returns:
+        The byte array representation of the image.
+    """
     img_byte_arr = io.BytesIO()
     image.save(img_byte_arr, format="PNG")
     return img_byte_arr
