b6f6aca12e79b9c47c5e0a732ba6a21553e27f2c
Santi G. <24705975+santiagoa58@users.noreply.github.com>
2023-12-23 22:04:27 +0000

    adds route_hash for deployments

--- terraform/modules/routes/outputs.tf
+++ terraform/modules/routes/outputs.tf
@@ -5,3 +5,7 @@ output "route_id" {
 output "route_uri" {
   value = aws_apigatewayv2_route.lemnispace_services_route.target
 }
+
+output "route_hash" {
+  value = sha1(jsonencode(aws_apigatewayv2_route.lemnispace_services_route))
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,24 @@
 output "TxtMosaicFunction_name" {
   description = "The name of the lambda function"
-  value = aws_lambda_function.TxtMosaicFunction.function_name
+  value       = aws_lambda_function.TxtMosaicFunction.function_name
 }
 
 output "TxtMosaicFunction_arn" {
   description = "The ARN of the lambda function"
-  value = aws_lambda_function.TxtMosaicFunction.arn
+  value       = aws_lambda_function.TxtMosaicFunction.arn
+}
+
+output "mosaic_route_id" {
+  description = "The ID of the mosaic service route"
+  value       = module.mosaic_route.route_id
+}
+
+output "mosaic_route_uri" {
+  description = "The URI of the mosaic service route"
+  value       = module.mosaic_route.route_uri
+}
+
+output "mosaic_route_hash" {
+  description = "The hash of the mosaic service route. Used for deployments to trigger when the route changes"
+  value       = module.mosaic_route.route_hash
 }
